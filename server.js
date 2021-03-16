ef6f4c1310d649cefacd540e5b985bc58dc2af2b
Diyar Kudrat <dkudrat@gmail.com>
2021-03-15 17:53:52 -0700

    backend server setup

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const socket = require('socket.io');
-const color = require('colors');
+const express = require("express");
+const socket = require("socket.io");
+const color = require("colors");
 
 const app = express();
 
+require('dotenv').config();
 
-const port = process.env.PORT;
-const server = app.listen(port, `Server is running in ${process.env.NODE_ENV} on ${port}`);
+
+const port = 8000;
+const server = app.listen(
+  port,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT} `
+      .yellow.bold
+  )
+);
 
 const io = socket(server);
+
