e85cc7491d9f5622b81216f651b8bb13c20ef7c6
Diyar Kudrat <dkudrat@gmail.com>
2021-03-16 10:27:13 -0700

    created methods that handles users joining a room to chat

---
+++ users.js
@@ -0,0 +1,29 @@
+const users = [];
+
+const userJoin = (id, username, room) => {
+    const user = { id, username, room };
+
+    users.push(user);
+}
+
+const getCurrentUser = (id) => {
+    const currUser = users.find((user) => user.id === id);
+
+    return currUser;
+}
+
+const userLeave = (id) => {
+    const idx = users.findIndex((user) => user.id === id);
+
+    if (idx !== -1) {
+        const user = users.splice(idx, 1)[0];
+
+        return user;
+    }
+}
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    userLeave
+}
