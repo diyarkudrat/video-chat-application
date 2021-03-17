c8823082ba2f0e6bf5af2d717b49482d90148e8b
Diyar Kudrat <dkudrat@gmail.com>
2021-03-17 10:53:02 -0700

    created Homepage component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './styles/home.css';
 
-function Home(props) {
+function Home({ socket }) {
+    const [username, setUsername] = useState('');
+    const [room, setRoom] = useState('');
+
+    const sendData = () => {
+        if (username !== '' && room !== '') {
+            socket.emit("join", { username, room });
+        } else {
+            alert("Username and Room Name must be filled!")
+        }
+    };
+
     return (
-        <div>
-            
+        <div className="homepage">
+            <h1>Video Chat Application</h1>
+
+            <input
+              placeholder="Enter Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              placeholder="Enter Room Name"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <Link to={`/chat/${room}/${username}`}>
+                <button onClick={sendData}>Join</button>
+            </Link>
         </div>
     );
 }
