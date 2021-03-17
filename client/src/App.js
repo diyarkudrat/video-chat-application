7a6038471f3b07bd6a4d28b138d46bcb6e5b8474
Diyar Kudrat <dkudrat@gmail.com>
2021-03-17 11:27:19 -0700

    fixed cors issue when connecting socket to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import io from 'socket.io-client';
+const ENDPOINT = "http://localhost:8000";
+const socket = io(ENDPOINT);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home socket={socket} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
