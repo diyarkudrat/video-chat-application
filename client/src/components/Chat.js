496a74889f6b5ba3c4f725d9b55fe36686fbff84
Diyar Kudrat <dkudrat@gmail.com>
2021-03-17 14:04:10 -0700

    created chatroom component and handling the encryption with messages

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,9 +1,42 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { process } from '../store/action/index';
+import { txtEncrypt, txtDecrypt } from '../utils/aes';
+import './styles/chat.css';
 
 
-function Chat(props) {
+function Chat({ socket, username, room }) {
+    const [text, setText] = useState('');
+    const [messages, setMessages] = useState([]);
+    const dispatch = useDispatch();
+
+    const dispatchProcess = (encrypt, msg, cipher) => {
+        dispatch(process(encrypt, msg, cipher));
+    };
+
+    useEffect(() => {
+        socket.on("message", (data) => {
+            const msg = txtDecrypt(data.message, data.username);
+            dispatchProcess(false, msg, data.message);
+
+            let temp = messages;
+            temp.push({
+                userId: data.userId,
+                username: data.username,
+                message: msg,
+            });
+            setMessages([...temp]);
+        });
+    }, [socket]);
+
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(scrollToBottom, "mess");
+
     return (
         <div>
             
