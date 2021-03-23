a1b6dab12bc2c0dcca8b4dd38c69c69c35dd5d58
Diyar Kudrat <dkudrat@gmail.com>
2021-03-23 10:37:42 -0700

    implemented lottie loading animations to display component

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './styles/display.css';
+import Lottie from 'react-lottie';
+import animationData from "../loading.json";
 
 
 function Display(props) {
     const [play, setPlay] = useState(false);
-
     const state = useSelector((state) => state.ProcessReducer);
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
 
     return (
         <div className="main">
@@ -17,6 +27,12 @@ function Display(props) {
                 <h3>Incoming Data</h3>
                 <p>{state.cypher}</p>
             </div>
+            <Lottie
+                options={defaultOptions}
+                height={150}
+                width={150}
+                isStopped={play}
+            />
             <div className="decrypt">
                 <h3>Decrypted Data</h3>
                 <p>{state.message}</p>
